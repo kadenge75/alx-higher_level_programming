00b0c11e400fca56f29aa86b4b588e9eb884b0f3
Mike kadenge <kadenge75@gmail.com>
2023-10-30 22:20:46 -0700

    pythonclasses

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
-
 
 class Rectangle:
-    """Represent a rectangle."""
-    pass
-~
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    # Example usage of the Rectangle class
+    rectangle = Rectangle()
+    # You can add properties and methods to the Rectangle class as needed.
